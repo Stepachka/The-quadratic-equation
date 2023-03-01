7d5891bf5e09cfbb37df736adfecb55c6dcacc09
stepachka <stepan.talabaev@mail.ru>
2023-03-02 02:48:48 +0300

    init repo

---
+++ lab1/Program.cs
@@ -0,0 +1,25 @@
+﻿using lab1.Models;
+
+namespace lab1
+{
+    public class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            
+            var Equation = new Equation();
+            var Answer = Equation.SolveEquationDiscriminant();
+            
+            if (Answer.decision)
+            {
+                Console.WriteLine($"Решение: х = {Answer.x1}, x = {Answer.x2}");
+            }
+            else
+            {
+                Console.WriteLine("Решения нет");
+            }
+        }
+
+    }
+}
